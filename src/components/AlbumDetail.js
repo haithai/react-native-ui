6f6b01eca2e0c06f9430764bc890a5c963a1c877
Hai Thai <hai.thai@hotschedules.com>
2018-08-04 15:43:58 +0700

    Finished Album List

---
+++ src/components/AlbumDetail.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { Text, View, Image } from 'react-native';
+import Card from './Card';
+import CardSelection from './CardSelection';
+
+const AlbumDetail = ({ album }) => {
+  const { title, artist, thumbnail_image, image } = album;
+  return (
+    <Card>
+      <CardSelection>
+        <View style={styles.thumbnailContainerStyle}>
+          <Image style={styles.thumbnailStyle} source={{ uri: thumbnail_image }} />
+        </View>
+        <View style={styles.headerContentStyle}>
+          <Text style={styles.headerTextStyle}>{title}</Text>
+          <Text>{artist}</Text>
+        </View>
+      </CardSelection>
+      <CardSelection>
+        <Image style={styles.imageStyle} source={{ uri: image }} />
+      </CardSelection>
+    </Card>
+  );
+};
+
+const styles = {
+  headerContentStyle: {
+    flexDirection: 'column',
+    justifyContent: 'space-around',
+  },
+  headerTextStyle: {
+    fontSize: 18,
+  },
+  thumbnailStyle: {
+    height: 50,
+    width: 50,
+  },
+  thumbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 5,
+    marginRight: 5,
+  },
+  imageStyle: {
+    height: 300,
+    flex: 1,
+  },
+};
+
+export default AlbumDetail;
---
+++ src/components/AlbumList.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import axios from 'axios';
+import AlbumDetail from './AlbumDetail';
+import Card from './Card';
+
+type Props = {}
+
+class AlbumList extends Component<Props> {
+  state = { albums: [] };
+
+  componentWillMount() {
+    console.log('componentWillMount in AlbumList');
+    axios.get('http://rallycoding.herokuapp.com/api/music_albums').then((res) => {
+      this.setState({ albums: res.data });
+    });
+  }
+
+  renderAlbums() {
+    const { albums } = this.state;
+    return albums.map(album => <Card><AlbumDetail album={album} /></Card>);
+  }
+
+  render() {
+    return (
+      <View>
+        {this.renderAlbums()}
+      </View>
+    );
+  }
+}
+
+export default AlbumList;
